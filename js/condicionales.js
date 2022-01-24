fd7aee8d3de233e5f0ad100403b96597324314f2
Rocio Olszevicki <86808467+Rosszevick@users.noreply.github.com>
2022-01-24 17:57:22 -0300

    nuevo proyecto

---
+++ js/condicionales.js
@@ -0,0 +1,10 @@
+let user = 0;
+if (user == "Tobias" && contraseña == "Tobias22"   ||
+    "Tammara" && pass == "Tammy33"||
+    "Tarico" && pass == "Tabaco5s" ){
+}else{
+    
+}
+localStorage.setItem ("user", "email", "contraseña")
+    
+    
---
+++ js/validacion.js
@@ -0,0 +1,16 @@
+document.querySelector("#boton").addEventListener('click',fconfirmacion)
+
+function fconfirmacion(){
+    let confirmacionBloque = document.querySelector("form");
+
+    confirmacionBloque.innerHTML = `Iniciando sesion...`;
+
+}
+
+ //document.querySelectorAll("form")             
+ //boton.addEventListener('click',confirmacion)
+    //function confirmacion(){
+   //botoninnerHTML = `<div>Iniciando sesion</div>`;
+
+ //}
+
